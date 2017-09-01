d809743bc17375e219e164255ee26348f1354512
ComaToastUK <jesusmanyouatethedog@gmail.com>
2017-09-01 13:31:15 +0100

    1 + 2 completed

--- week1/hash2.rb
+++ week1/hash2.rb
@@ -1,5 +1,9 @@
-def sort_hash_values(h)
-  
+def sort_hash_values(hash)
+  x = hash.keys
+  y = hash.values.sort
+  [x,y].transpose.to_h
+  # new_hash = hash.inject { |hash, (k,v)| hash[k] v.sort}
+  # new_hash
 end
 p sort_hash_values({ a: 2, b: 5, c: 1 })
-# { a: 2, b: 5, c: 1 } => { a: 1, b: 2, c: 5 }
+ # { a: 2, b: 5, c: 1 } => { a: 1, b: 2, c: 5 }
--- week1/hash3.rb
+++ week1/hash3.rb
@@ -1,5 +1,5 @@
 def sorted_array(h)
-  h.values.sort
+  h.values.sort.to_a
 end
 p sorted_array({a:2, b:5, c:1})
 # { a: 2, b: 5, c: 1 } => [1, 2, 5]
